bec81a7eba32da2665cffb82047ef3bf8e4dadd4
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-04-09 20:04:46 +0530

    created third intro screen

--- lib/screens/intro_screens/intro_page3.dart
+++ lib/screens/intro_screens/intro_page3.dart
@@ -1,4 +1,7 @@
+import 'package:brahma/widgets/body_text.dart';
+import 'package:brahma/widgets/head_text.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class IntroPage3 extends StatelessWidget {
   const IntroPage3({super.key});
@@ -9,7 +12,36 @@ class IntroPage3 extends StatelessWidget {
       backgroundColor: const Color(0XFFF2DACB),
       body: Center(
         child: Column(
-          children: const [],
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Lottie.network(
+                  'https://assets7.lottiefiles.com/packages/lf20_jqenj9df.json'),
+            ),
+            const SizedBox(
+              height: 170,
+            ),
+            const HeadText(headText: 'Why Ads??'),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              padding: const EdgeInsets.all(30),
+              child: const BodyText(
+                  bodyText:
+                      "Open AI API is expensive to keep this app free, I need to pay for the API. That's why ads are there, You can always buy the pro version if you want or enter your own api key to go ad-free forever"),
+            ),
+            const BodyText(bodyText: 'Made with ❤️ by Utkarsh Shrivastava'),
+            ElevatedButton(
+
+              onPressed: () {},
+              child: const Text('Support by giving it a star on github'),
+            ),
+          ],
         ),
       ),
     );
