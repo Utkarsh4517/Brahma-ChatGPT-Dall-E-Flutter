de9760017c6ea06f9d3651dab1731fc5d510ded4
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-04-12 19:52:43 +0530

    made google sign in work, also every form of authentication is working now

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,21 +4,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
+
   signInWithGoogle() async {
+    // begin interactive sign in progress
+  final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
 
-    // begin interactive sign in process
-    final GoogleSignInAccount ? gUser = await GoogleSignIn().signIn();
+    // obtain auth details from request 
+  final GoogleSignInAuthentication gAuth = await gUser!.authentication;
 
-    // obtain auth details from request
-    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
-
-    // create new credential for user
-    final credential = GoogleAuthProvider.credential(
-      accessToken: gAuth.accessToken,
-      idToken: gAuth.idToken,
-    );
-
-    // final lets sign in
+    // create a new credential for user
+  final credential = GoogleAuthProvider.credential(
+    accessToken: gAuth.accessToken,
+    idToken: gAuth.idToken
+  );
+    // finally lets sign in 
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
+
 }
