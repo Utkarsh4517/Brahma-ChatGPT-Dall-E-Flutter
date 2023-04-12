ff0e79eb11a205ded8d33581e38179ca03f8c836
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-04-12 12:45:00 +0530

    added user display name on ChatScreen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -88,7 +88,7 @@ class _ChatScreenState extends State<ChatScreen> {
               child: TextAndVoiceField(),
             ),
             const BodyText(bodyText: 'Swipe right to Generate Image'),
-            Text(user.email!)
+            Text(user.displayName!)
           ],
         ),
       ),
